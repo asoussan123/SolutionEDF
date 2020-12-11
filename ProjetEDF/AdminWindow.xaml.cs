fe97a2f3c4315584646d2f86317b32b41f60c5a9
Alexis SOUSSAN <asoussan@lerebours.org>
2020-12-11 12:00:32 +0100

    Ajoutez des fichiers projet.

---
+++ ProjetEDF/AdminWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetEDF
+{
+    /// <summary>
+    /// Logique d'interaction pour AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        edfEntities gst;
+        int idCont;
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            gst = new edfEntities();
+            lstControleurs.ItemsSource = gst.controleur.ToList();
+        }
+
+        private void lstControleurs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(lstControleurs.SelectedItem != null)
+            {
+                idCont = (lstControleurs.SelectedItem as controleur).id;
+                lstClients.ItemsSource = (from cl in gst.client
+                                         where gst.controleur.Any(i => i.id == cl.identifiant && i.id == idCont)
+                                         select cl).ToList<client>();
+
+
+                int ancien = (lstClients.SelectedItem as client).ancienReleve;
+                int dernier = (lstClients.SelectedItem as client).dernierReleve;
+                int different = dernier - ancien;
+
+            }
+        }
+    }
+}
---
+++ ProjetEDF/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetEDF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        edfEntities gst;
+        controleur ctrl;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            gst = new edfEntities();
+            controleur leCtrl = gst.controleur.ToList().Find(ctrl => ctrl.login == txtlogin.Text && ctrl.mdp == txtMdp.Text);
+        }
+        
+
+        
+        
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            AdminWindow frm = new AdminWindow();
+            frm.Show();
+            //if(txtlogin.Text == "")
+            //{
+            //    txtErreur.Text = "Saisir le login";
+            //}
+            //else
+            //{
+            //    if(txtMdp.Text == "")
+            //    {
+            //        txtErreur.Text = "Saisir le mdp";
+            //    }
+            //    else
+            //    {
+            //        if(txtlogin.Text == ctrl.login && txtMdp.Text == ctrl.mdp)
+            //        {
+            //            if(ctrl.statut == "admin")
+            //            {
+            //                AdminWindow frm = new AdminWindow();
+            //                frm.Show();
+            //            }
+            //            else
+            //            {
+            //                CtrlWindow frm = new CtrlWindow();
+            //                frm.Show();
+            //            }
+            //        }
+            //        else
+            //        {
+            //            txtErreur.Text = "Login ou mdp incorrecte";
+            //        }
+            //    }
+        }
+    }
+    
+}
